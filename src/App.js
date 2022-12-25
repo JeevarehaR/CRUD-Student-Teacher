cc855856ad1b6b819e8736ad7807110742b0d839
JeevarehaR <jeev.reh@gmail.com>
2022-12-25 16:35:54 +0530

    CRUD added

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home";
+import NotFound from "./Components/NotFound";
+import Student from "./Components/Student/Student";
+import AddStudent from "./Components/Student/AddStudent";
+import { EditStudent } from "./Components/Student/EditStudent";
+import Mentor from "./Components/Mentor/Mentor";
+import AddMentor from "./Components/Mentor/AddMentor";
+import { EditMentor } from "./Components/Mentor/EditMentor";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/student" element={<Student />} />
+        <Route path="/addstudent" element={<AddStudent />} />
+        <Route path="student/editstudent/:id" element={<EditStudent />} />
+        <Route path="/mentor" element={<Mentor />} />
+        <Route path="/addmentor" element={<AddMentor />} />
+        <Route path="mentor/editmentor/:id" element={<EditMentor />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
